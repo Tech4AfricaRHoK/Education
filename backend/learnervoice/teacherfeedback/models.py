422a3f5dfb7b9f2cfa143c03015cff62886d467e
Thane Thomson <connect@thanethomson.com>
2015-10-06 12:32:28 +0200

    tweaked more of the model structure

--- backend/learnervoice/teacherfeedback/models.py
+++ backend/learnervoice/teacherfeedback/models.py
@@ -29,6 +29,8 @@ class Profile(models.Model):
     surname = models.CharField(max_length=255, help_text=_("The person's last name"))
     mobile = models.CharField(max_length=100, unique=True, help_text=_("The person's mobile phone number."))
     profile_type = models.CharField(max_length=30, choices=Profile.TYPES, default=Profile.STUDENT)
+    school = models.ForeignKey('School', null=True, blank=True, help_text=_("The school for this user, if any"))
+    grade = models.ForeignKey('Grade', null=True, blank=True, help_text=_("The grade for this user, if any"))
 
     def __unicode__(self):
         return "%s %s" % (self.name, self.surname)
@@ -42,25 +44,33 @@ class School(models.Model):
     province = models.IntegerField(help_text=_("School's province"))
     country = models.IntegerField(help_text=_("School's country"))
 
-
-class Grade(models.Model):
-    number = models.IntegerField(help_text=_("Grade number"))
+    def __unicode__(self):
+        return "%s" % self.name
 
 
 class Subject(models.Model):
-    name = models.CharField(max_length=255, help_text=_("Name of a subject"))
+    name = models.CharField(max_length=255, help_text=_("Name of a subject"), unique=True)
+
+    return __unicode__(self):
+        return self.name
 
 
-class GradeSubject(models.Model):
-    grade = models.ForeignKey(Grade)
+class TeacherSubjectGrade(models.Model):
+    """
+    A particular subject taught by a particular teacher, for a specific grade and school.
+    """
+    teacher = models.ForeignKey(Profile)
     subject = models.ForeignKey(Subject)
+    grade = models.IntegerField(min_value=1, max_value=12)
+    year = models.IntegerField(min_value=2015, help_text=_("The year for which this subject is relevant"))
+    school = models.ForeignKey(School)
 
 
 class Rating(models.Model):
     """
     For when a student wants to rate a particular teacher.
     """
-    value = models.IntegerField(help_text=_("Rating given to a teacher"))
+    value = models.IntegerField(help_text=_("Rating given to a teacher"), min_value=1, max_value=5)
     comment = models.CharField(max_length=1000, help_text=_("Comment associated with a rating"))
     teacher = models.ForeignKey(Profile, help_text=_("The teacher associated with this rating"))
     student = models.ForeignKey(Profile, help_text=_("The student associated with this rating"))
