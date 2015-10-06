221f54282603bc394d4e40a4033bf5bc3fcbca1f
Thane Thomson <connect@thanethomson.com>
2015-10-06 10:41:19 +0200

    added app to settings, added first model (Profile)

--- backend/learnervoice/teacherfeedback/models.py
+++ backend/learnervoice/teacherfeedback/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Every user in the system has a profile, but not every profile has a user
+    associated with it.
+    """
+    user = models.ForeignKey(User, blank=True, null=True,
+            help_text=_("The (optional) user account associated with this profile."))
+    name = models.CharField(max_length=255, help_text=_("The person's first name"))
+    surname = models.CharField(max_length=255, help_text=_("The person's last name"))
+
