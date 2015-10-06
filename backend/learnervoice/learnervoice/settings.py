fa5789d0ec5921376345ee040baddc04f6569b69
Thane Thomson <connect@thanethomson.com>
2015-10-06 11:22:50 +0200

    replaced tastypie with rest_framework

--- backend/learnervoice/learnervoice/settings.py
+++ backend/learnervoice/learnervoice/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tastypie',
+    'haystack',
+    'rest_framework',
     'teacherfeedback',
 )
 
