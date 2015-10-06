4db03b09233d3ab81e7f4dac9671b7b5c513503c
Thane Thomson <connect@thanethomson.com>
2015-10-06 11:58:16 +0200

    added more fields to profile model

--- backend/learnervoice/teacherfeedback/models.py
+++ backend/learnervoice/teacherfeedback/models.py
@@ -8,8 +8,27 @@ class Profile(models.Model):
     Every user in the system has a profile, but not every profile has a user
     associated with it.
     """
+    STUDENT = 'student'
+    TEACHER = 'teacher'
+    PARENT = 'parent'
+    PRINCIPAL = 'principal'
+    ADMIN = 'admin'
+
+    TYPES = (
+        (STUDENT, _('Student')),
+        (TEACHER, _('Teacher')),
+        (PARENT, _('Parent')),
+        (PRINCIPAL, _('Principal')),
+        (ADMIN, _('Admin')),
+    )
+
     user = models.ForeignKey(User, blank=True, null=True,
             help_text=_("The (optional) user account associated with this profile."))
     name = models.CharField(max_length=255, help_text=_("The person's first name"))
     surname = models.CharField(max_length=255, help_text=_("The person's last name"))
+    mobile = models.CharField(max_length=100, unique=True, help_text=_("The person's mobile phone number."))
+    profile_type = models.CharField(max_length=30, choices=Profile.TYPES, default=Profile.STUDENT)
+
+    def __unicode__(self):
+        return "%s %s" % (self.name, self.surname)
 
