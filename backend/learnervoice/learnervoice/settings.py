b807f450d5be1b7da395a51d51ad68d67cf1ba35
Thane Thomson <connect@thanethomson.com>
2015-10-06 10:18:11 +0200

    added teacherfeedback app, added mysql config for dev

--- backend/learnervoice/learnervoice/settings.py
+++ backend/learnervoice/learnervoice/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tastypie',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'learnervoice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learnervoice',
+        'USER': 'learnervoice',
+        'PASSWORD': 'learnervoice',
     }
 }
 
@@ -85,9 +88,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Johannesburg'
 
 USE_I18N = True
 
